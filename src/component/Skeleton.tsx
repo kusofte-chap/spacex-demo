e499a867d6b05e20abac2696e5d938c01e3f9ddd
kusofte-chap <969211472@qq.com>
2023-05-29 11:17:12 +0800

    fixed:移动端骨架屏显示问题

--- src/component/Skeleton.tsx
+++ src/component/Skeleton.tsx
@@ -1,15 +1,19 @@
 import Grid from '@mui/material/Unstable_Grid2';
 import Box from '@mui/material/Box';
 import Skeleton from '@mui/material/Skeleton';
+import {useMediaQuery, useTheme} from "@mui/material";
 
 
 export default function SkeletonLayout() {
+    const theme = useTheme()
+    const match = useMediaQuery(theme.breakpoints.down('md'))
+
     return (
         <Box sx={{overflow: 'hidden',width: '100%',px:1.5,pt:2}} >
-            <Grid container rowSpacing={4} columnSpacing={{md: 3}}>
+            <Grid container rowSpacing={3} columnSpacing={{md: 3}}>
                 {Array.from(new Array(6)).map((_, index) => (
-                    <Grid sm={12} md={6} key={index} >
-                        <Skeleton variant="rectangular"  height={210} />
+                    <Grid sm={12} md={6} key={index} sx={{width:match ? '100%':'auto'}}>
+                        <Skeleton variant="rectangular" width='100%' height={210} />
                         <Box sx={{pt: 0.5}}>
                             <Skeleton/>
                             <Skeleton width="60%"/>
